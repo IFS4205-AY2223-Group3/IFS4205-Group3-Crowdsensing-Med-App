154e4da7b83831653e12352ca682531bd69e051c
IsaacLai <e0540509@u.nus.edu>
2022-09-09 20:59:20 +0800

    Add authentication

---
+++ backend/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect(reverse('success'))
+		else:
+			messages.success(request, ("There Was An Error Logging In, Try Again..."))	
+			return redirect(reverse('index'))
+
+	else:
+		return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You Were Logged Out!"))
+	return redirect('index')
