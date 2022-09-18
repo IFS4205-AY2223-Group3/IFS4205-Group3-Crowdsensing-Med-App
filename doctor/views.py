f4cffba8a47bfa636521697bf92737eed3a720d4
Daryl <ithinkmynameisdaryl@gmail.com>
2022-09-18 23:39:11 +0800

    Move some logic to backend and add some code to handle unauthorized actions

--- doctor/views.py
+++ doctor/views.py
@@ -1,43 +1,46 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
-from backend.models import Patient, Doctor, HealthRecords, PendingSessions, Examinations
+from backend.models import Doctor, HealthRecords, PendingSessions, Examinations
+from backend.views import check_user_role, check_session_approved, get_examination_context
 
 # Create your views here.
     
 def dashboard(request):
     if (request.user.is_authenticated):
+        check_user_role(request, Doctor)
         return render(request, 'doctor/dashboard.html')
     else:
         return redirect(reverse('login:index'))
     
 def new_session(request):
     if (request.user.is_authenticated):
+        check_user_role(request, Doctor)
         return render(request, 'doctor/newsession.html')
     else:
         return redirect(reverse('login:index'))
 
 def examine(request):
     if (request.user.is_authenticated):
-        try:
-            doc = Doctor.objects.get(user=request.user)
-            new_session = PendingSessions.objects.get(doctor=doc)
-            if new_session.approved == False:
-                request.session['error_msg'] = 'Patient has not approved the request!'
-                return redirect(reverse('doctor:confirmation'))
-            context = { 
-                'session': new_session, 
-                'record': HealthRecords.objects.get(user=new_session.patient),
-                'past_visits': Examinations.objects.all().filter(patient=new_session.patient)
-                }
+        check_user_role(request, Doctor)
+        if check_session_approved(request) is True:
+            context = get_examination_context(request)
             return render(request, 'doctor/examine.html', context)
-        except Doctor.DoesNotExist:
-            return redirect(reverse('login:index'))
+        else:
+            request.session['error_msg'] = 'Patient has not given consent!'
+            return redirect(reverse('doctor:confirmation'))
     else:
         return redirect(reverse('login:index'))
 
 def confirmation(request):
     if (request.user.is_authenticated):
+        check_user_role(request, Doctor)
+
+        if 'session_id' not in request.session:
+            return redirect(reverse('doctor:newsession'))
+        if 'error_msg' not in request.session:
+            request.session['error_msg'] = ''
+
         context = {
             'sessionid': request.session['session_id'],
             'error_message': request.session['error_msg']
