c329b60061638e809b880d4d34b6053f42c0ba5a
Daryl <ithinkmynameisdaryl@gmail.com>
2022-09-20 15:03:43 +0800

    Update project structure and add rest_framework to installed apps

--- backend/urls.py
+++ backend/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 app_name = 'backend'
 urlpatterns = [
-    path('login_user', views.login_user, name='login'),
-    path('logout_user', views.logout_user, name='logout'),
-    path('create_session', views.create_session, name='createsession'),
-    path('assign_doctor', views.assign_doctor, name='assigndoctor'),
-    path('submit_examination', views.finish_examine, name='submit_examination'),
-    path('get_records', views.get_records, name='getrecords'),
-    path('get_sessions', views.get_sessions, name='getsessions')
+    #path('login_user', views.login_user, name='login'),
+    #path('logout_user', views.logout_user, name='logout'),
+    #path('create_session', views.create_session, name='createsession'),
+    #path('assign_doctor', views.assign_doctor, name='assigndoctor'),
+    #path('submit_examination', views.finish_examine, name='submit_examination'),
+    #path('get_records', views.get_records, name='getrecords'),
+    #path('get_sessions', views.get_sessions, name='getsessions')
 ]
