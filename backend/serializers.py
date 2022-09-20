b8b7fe8df7bc4a73d1e8f1820fde3cdd6ac9e9ec
Daryl <ithinkmynameisdaryl@gmail.com>
2022-09-20 23:49:51 +0800

    Add serializers for pendingsessions and examinations

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
 from backend.models import *
+
+class PendingSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PendingSessions
+        fields = ['patient']
+
+class ExaminationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Examinations
+        fields = ['session_id', 'doctor', 'patient', 'diagnosis', 'prescription', 'sessiontime']
