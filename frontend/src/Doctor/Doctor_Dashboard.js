755afbe580bb0869ce5a9b316c7c097215c354ea
Oscar Lai <77140576+oscarlai1998@users.noreply.github.com>
2022-09-22 02:15:28 +0800

    Finished login and authentication handling
    Todo: do redirection after logout

--- frontend/src/Doctor/Doctor_Dashboard.js
+++ frontend/src/Doctor/Doctor_Dashboard.js
@@ -1,8 +1,19 @@
+import { useAuth } from "../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+const Signout = () => {
+  const { logout } = useAuth();
+  // const navigate = useNavigate();
+
+  logout();
+};
+
 const Doctor_Dashboard = () => {
-    return (
-      <div>
-        <p>Welcome to your Dashboard</p>
-      </div>
-    );
-  };
-  export default Doctor_Dashboard;
+  return (
+    <div>
+      Welcome Doctor
+      <button onClick={Signout}>Sign out</button>
+    </div>
+  );
+};
+export default Doctor_Dashboard;
--- frontend/src/Patient/Patient_Dashboard.js
+++ frontend/src/Patient/Patient_Dashboard.js
@@ -1,8 +1,19 @@
+import { useAuth } from "../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+const Signout = () => {
+  const { logout } = useAuth();
+  // const navigate = useNavigate();
+
+  logout();
+  // navigate("../b/login");
+};
 const Patient_Dashboard = () => {
-    return (
-      <div>
-        <p>Welcome to your Dashboard</p>
-      </div>
-    );
-  };
-  export default Patient_Dashboard;
+  return (
+    <div>
+      Welcome Patient
+      <button onClick={Signout}>Sign out</button>
+    </div>
+  );
+};
+export default Patient_Dashboard;
