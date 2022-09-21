61104e357d9cbff48a4f478741b43477db988748
Oscar Lai <77140576+oscarlai1998@users.noreply.github.com>
2022-09-21 15:07:35 +0800

    First Commit
    - Created login page

---
+++ frontend/src/Doctor/Doctor_Dashboard.js
@@ -0,0 +1,8 @@
+const Doctor_Dashboard = () => {
+    return (
+      <div>
+        <p>Welcome to your Dashboard</p>
+      </div>
+    );
+  };
+  export default Doctor_Dashboard;
---
+++ frontend/src/Patient/Patient_Dashboard.js
@@ -0,0 +1,8 @@
+const Patient_Dashboard = () => {
+    return (
+      <div>
+        <p>Welcome to your Dashboard</p>
+      </div>
+    );
+  };
+  export default Patient_Dashboard;
