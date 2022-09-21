96495e3939c11123541e79e7577b57bf0dd02b3a
Oscar Lai <77140576+oscarlai1998@users.noreply.github.com>
2022-09-22 02:15:28 +0800

    Finished login and authentication handling
    Todo: do redirection after logout

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -1,145 +1,108 @@
-import { useRef, useState, useEffect, useContext } from 'react';
-import AuthContext from "../context/AuthProvider";
-import axios from 'axios';
-import './Login.css'
-import * as constantClass from '../api/constants'
-
-const LOGIN_URL = constantClass.LOGIN_URL;
+import { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import { useAuth } from "../context/AuthProvider";
 
 const Login = () => {
-    const { setAuth } = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
+  const { login } = useAuth();
 
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [userRole, setRole] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  const userRef = useRef();
+  const errRef = useRef();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [userRole, setRole] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd])
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
 
-    //    // Comment out to test success
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    //     // Getting response 
-    //     const accessToken = "response?.data?.accessToken";
-    //     const userRole = "patient";
-    //     const name = "John"
-    //     const userId = "response?.data.userId";
+    const data = {
+      user: user,
+      pwd: pwd,
+      userRole: userRole,
+    };
 
-    //     // Auth Token
-    //     setAuth({ userId, name, userRole, accessToken });
-    //     setUser('');
-    //     setPwd('');
-    //     setRole('');
-    //     setSuccess(true);
+    const response = await login(data);
 
-        
-        
-
-        // try {
-        //     const response = await axios.post(LOGIN_URL,
-        //         JSON.stringify({ user, pwd, userRole }),
-        //         {
-        //             headers: { 'Content-Type': 'application/json' },
-        //             withCredentials: true
-        //         }
-        //     );
-        //     console.log(JSON.stringify(response?.data));
-        //     //console.log(JSON.stringify(response));
-
-        //     // Getting response 
-        //     const accessToken = response?.data?.accessToken;
-        //     const userRole = response?.data?.role;
-        //     const name = response?.data?.name;
-        //     const userId = response?.data.userId;
-
-        //     // Auth Token
-        //     setAuth({ userId, name, userRole, accessToken });
-        //     setUser('');
-        //     setPwd('');
-        //     setRole('');
-        //     setSuccess(true);
-
-
-        // } catch (err) {
-        //     if (!err?.response) {
-        //         setErrMsg('No Server Response');
-        //         // setSuccess(true);
-
-        //     } else if (err.response?.status === 400) {
-        //         setErrMsg('Missing Username or Password');
-        //     } else if (err.response?.status === 401) {
-        //         setErrMsg('Unauthorized');
-        //     } else {
-        //         setErrMsg('Login Failed');
-        //     }
-        //     errRef.current.focus();
-        // }
+    if (response.statusCode === 200) {
+      const role = localStorage.getItem("userRole");
+      if (role === "patient") {
+        navigate("/patient");
+      } else if (role === "doctor") {
+        navigate("/doctor");
+      } else {
+        navigate("");
+      }
+    } else if (response.statusCode === 400) {
+      setErrMsg("Invalid Username and Password");
+    } else if (response.statusCode === 500) {
+      setErrMsg("No Server Response");
+    } else {
+      setErrMsg("Login Failed. Contact Server Admin");
     }
+  };
 
-    return (
-        <>
-            {success ? (
-                <section>
-                    <h1>You are logged in!</h1>
-                    <br />
-                    <p>
-                        <a href="#">Go to Home</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>IFS4205 Group 3 Medical Application</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                        />
+  return (
+    <div className="container">
+      <section>
+        <p
+          ref={errRef}
+          className={errMsg ? "errmsg" : "offscreen"}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
+        <h1>IFS4205 Group 3 Medical Application</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+          />
 
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
-                        <label htmlFor="role">Role: </label>
-                            <select 
-                                value={userRole} 
-                                name="roleDropdown"
-                                onChange={(e) => setRole(e.target.value)}
-                                required>
-                            <option disabled selected value="">Select Role</option>
-                            <option value="patient">Patient</option>
-                            <option value="doctor">Doctor</option>
-                            <option value="medicalStaff">Medical Helper</option>
-                            <option value="researcher">Researcher</option>
-                        </select>
-                        <button>Sign In</button>
-                    </form>
-                </section>
-            )}
-        </>
-    )
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+          />
+          <label htmlFor="role">Role: </label>
+          <select
+            value={userRole}
+            name="roleDropdown"
+            onChange={(e) => setRole(e.target.value)}
+            required
+          >
+            <option disabled selected value="">
+              Select Role
+            </option>
+            <option value="patient">Patient</option>
+            <option value="doctor">Doctor</option>
+            <option value="medicalStaff">Medical Helper</option>
+            <option value="researcher">Researcher</option>
+          </select>
+          <button>Sign In</button>
+        </form>
+      </section>
+    </div>
+  );
+};
 
-}
-
-export default Login
+export default Login;
