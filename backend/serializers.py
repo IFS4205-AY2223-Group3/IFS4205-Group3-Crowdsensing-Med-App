d01a7db0b019a64cdefb1c830b10fc8ef3b41e38
IsaacLai <e0540509@u.nus.edu>
2022-09-21 18:01:00 +0800

    Add working login API for users

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from backend.models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'name']
--- backend/views.py
+++ backend/views.py
@@ -1,53 +1,50 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.urls import reverse
-from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils.datastructures import MultiValueDictKeyError
 from django.http import HttpResponse
-from backend.models import *
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from backend.models import Patient, Doctor, Researcher, MedicalStaff, PendingSessions, HealthRecords, Examinations, User
+from backend.serializers import UserSerializer 
 
+@api_view(["POST", "GET"])
 def login_user(request):
 	if request.method == "POST":
-		username = request.POST['username']
-		password = request.POST['password']
-		try:
-			role = request.POST['role']
-		except MultiValueDictKeyError:
-			messages.error(request, ("Please select a role."))
-			return redirect(reverse('login:index'))
+		username = request.POST.get('user')
+		password = request.POST.get('password')
+		role = request.POST.get('role')
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
 			try:
-				user_role = get_role(role)
-				if user_role.objects.get(user_id=user.user_id):
-					login(request, user)
-					return redirect_user(role, request, user)
+				get_role(role).objects.get(pk=user.user_id)
+				login(request, user)
+				data = {}
+				data = UserSerializer(user).data
+				data["role"] = role
+				return Response(data, status=status.HTTP_200_OK)
 			except ObjectDoesNotExist:
-				messages.error(request, ("There was an error logging in, try again."))	
-				return redirect(reverse('login:index'))
-		else:
-			messages.error(request, ("There was an error logging in, try again."))	
-			return redirect(reverse('login:index'))
-
+				return Response({'errorMessage': 'Login credentials are incorrect. Please check and try again.'}
+				, status=status.HTTP_403_FORBIDDEN)
+		else:	
+			return Response({'errorMessage': 'Login credentials are incorrect. Please check and try again.'}
+			, status=status.HTTP_403_FORBIDDEN)
 	else:
-		return render(request, 'authenticate/login.html', {})
-
-def logout_user(request):
-	logout(request)
-	messages.success(request, ("You Were Logged Out!"))
-	return redirect('login:index')
+		return Response({'errorMessage': 'Invalid request method.'}, status=status.HTTP_400_BAD_REQUEST)
 
 def create_session(request):
 	if request.method == "POST":
-		username = request.POST['username']
+		user = request.user
 		session = PendingSessions.objects.create_session(username)
 		return HttpResponse(session.sessionid)
 
 def get_records(request):
 	if request.method == "GET":
 		personal_records = request.user
-		health_records = HealthRecords.objects.get(user_id=request.user.user_id)
+		health_records = HealthRecords.objects.get(userid=request.user.userid)
 		context= {
 			'health_records':health_records,
 			'personal_records': personal_records
@@ -66,37 +63,19 @@ def get_sessions(request):
 
 def assign_doctor(request):
 	if request.method == "POST":
-		sessionid = request.POST['session_id']
+		sessionid = request.POST['sessionid']
 		user = request.user
-		try:
-			doctor = Doctor.objects.get(user=user)
-			target_session = PendingSessions.objects.get(session_id=sessionid)
-			target_session.doctor = doctor
+		doctor = Doctor.objects.get(userid=user.userid)
+		if doctor is not None:
+			target_session = PendingSessions.objects.get(sessionid=sessionid)
+			target_session.doctorid = doctor
 			target_session.save()
-			request.session['session_id'] = target_session.session_id
-			request.session['error_msg'] = ''
+			request.session['sessionid'] = target_session.sessionid
 			return redirect(reverse('doctor:confirmation'))
-		except (Doctor.DoesNotExist, PendingSessions.DoesNotExist) as exception:
+		else:
 			return HttpResponse("invalid")
 	else:
 		return HttpResponse("invalid")
-
-def finish_examine(request):
-	if request.method == "POST":
-		# definitely need to add sanitization here
-		sessionid = request.session['session_id']
-		patient = Patient.objects.get(user_id=request.POST['patientid'])
-		doctor = Doctor.objects.get(user_id=request.POST['doctorid'])
-		diagnosis = Diagnosis.objects.get(code=request.POST['code'])
-		prescription = request.POST['prescription']
-		new_exam = Examinations.objects.create_exam(sessionid, doctor, patient, diagnosis, prescription)
-		new_exam.save()
-		
-		PendingSessions.objects.get(session_id=sessionid).delete()
-		del request.session['session_id']
-		del request.session['error_msg']
-		return redirect(reverse('doctor:newsession'))
-	return HttpResponse("huh")
 		
 def get_role(user_role):
 	role = user_role.lower()
@@ -108,43 +87,3 @@ def get_role(user_role):
 		return Researcher
 	if role == 'medicalstaff':
 		return MedicalStaff
-
-def redirect_user(role_string, request, user):
-	if (role_string == 'patient'):
-		return redirect(reverse('patient:dashboard'))
-	if (role_string == 'doctor'):
-		return redirect(reverse('doctor:dashboard'))
-	if (role_string == 'researcher'):
-		return redirect(reverse('researcher:dashboard'))
-	if (role_string == 'medicalstaff'):
-		return redirect(reverse('medicalstaff:dashboard'))
-
-def check_user_role(request, role):
-	try:
-		role.objects.get(user_id=request.user)
-	except role.DoesNotExist:
-		raise PermissionDenied
-
-def check_session_approved(request):
-	try:
-		session = PendingSessions.objects.get(doctor=request.user.user_id)
-		if session.approved is False:
-			return False
-		return True
-	except PendingSessions.DoesNotExist:
-		return False
-
-def get_examination_context(request):
-	session = PendingSessions.objects.get(doctor=request.user.user_id)
-	context = { 
-            'session': session, 
-            'record': HealthRecords.objects.get(user=session.patient),
-            'past_visits': Examinations.objects.all().filter(patient=session.patient)
-            }
-	return context
-
-# Common
-def get_name(request):
-	if request.method == "GET":
-		loggedin_user = get_object_or_404(User, userid=request.user.userid)
-		return loggedin_user.name
