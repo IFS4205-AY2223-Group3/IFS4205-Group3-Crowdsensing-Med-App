b48b1fb66612f01a9b176af4d68ef06a140655a1
Oscar Lai <77140576+oscarlai1998@users.noreply.github.com>
2022-09-24 02:58:07 +0800

    Changes
    -Added Allow Session and Generate Session Pages
    - Changed constants for local testing (to speed up rest api response)
    -

--- frontend/src/Patient/View_Records.js
+++ frontend/src/Patient/View_Records.js
@@ -1,13 +1,46 @@
 import { useAuth } from "../context/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import styles from "./Patient_Dashboard.module.css";
+import axios from "axios";
+import { VIEW_RECORDS_URL } from "../api/constants";
 
 const View_Records = () => {
   const navigate = useNavigate();
   const name = localStorage.getItem("name");
+  const userId = localStorage.getItem("userId");
+  const accessToken = localStorage.getItem("accessToken");
+
+  axios
+    .post(
+      VIEW_RECORDS_URL,
+      {
+        token: accessToken,
+        userId: userId,
+      },
+      {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      }
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  const Back = async () => {
+    navigate("/patient");
+  };
 
   return (
-    <div>
-      <h2>Viewing {name}'s Records</h2>
+    <div className={styles.container}>
+      <h2 className={styles.header}>Viewing Records for {name}!</h2>
+      <div class={styles.buttons_container}>
+        <button class={styles.button} onClick={Back}>
+          Back
+        </button>
+      </div>
     </div>
   );
 };
