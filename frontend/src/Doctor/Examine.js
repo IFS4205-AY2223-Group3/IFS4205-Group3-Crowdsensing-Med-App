7543c8579b33832787b86c50a72d303ce238f2b9
aliciatay-zls <e0424736@u.nus.edu>
2022-09-26 01:11:25 +0800

    Add popup, improve UI for examine page

--- frontend/src/Doctor/Examine.js
+++ frontend/src/Doctor/Examine.js
@@ -1,64 +1,63 @@
-import { useRef, useState, useEffect } from "react";
-import { useAuth } from "../context/AuthProvider";
+import React, { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DoctorApi } from "./DoctorAPI";
-import styles from "./Doctor_Dashboard.module.css";
-import logo from "../imports/medibook.png";
+import styles from "./Examine.module.css";
+import PopUp from "../PopUp";
 
 const Examine = () => {
 	const { send_examId } = DoctorApi();
 
-	const userRef = useRef();
-	const errRef = useRef();
   const navigate = useNavigate();
 
 	const [examId, setExamId] = useState("");
 	const [errMsg, setErrMsg] = useState("");
+	const [errPopUp, setErrPopUp] = useState(false);
 
 	const handleSubmit = async (e) => {
     e.preventDefault();
 
 		const data = {
 			examId: examId,
-		}
+		};
 
-		const response = await send_examId(data); //pass in data needed for POST request
+		const response = await send_examId(data);
 
 		if (response.statusCode === 200) {
-      const patientId = localStorage.getItem("patientId");
-      const patientName = localStorage.getItem("patientName");
-
 			navigate("/submitexamination");
     } else {
       setErrMsg(response.errorMessage);
+			setErrPopUp(true);
 		}
 	};
 
-  return (
-    <div className={styles.container}>
-			<section>
-				<p
-					ref={errRef}
-					className={errMsg ? "errmsg" : "offscreen"}
-					aria-live="assertive"
-				>
-				{errMsg}
-				</p>
-				<img src={logo} className={styles.logo} alt="logo" />
+	const togglePopUp = () => {
+		setErrPopUp(!errPopUp);
+	};
 
+	const Back = async () => {
+    navigate("/doctor");
+  };
+
+  return (		
+    <div className={styles.container}>
+			{errPopUp ? <PopUp toggle={togglePopUp} msg={errMsg}/> : null}
+
+			<section>
 				<h1>Enter Session ID to examine</h1>
 				<form onSubmit={handleSubmit}>
 					<label htmlFor="examId">Session ID:</label>
 					<input
 						type="text"
 						id="examId"
-						ref={userRef}
 						autoComplete="off"
 						onChange={(e) => setExamId(e.target.value)}
 						value={examId}
 						required
 					/>
+					<button>Enter</button>
 				</form>
+
+				<button class={styles.back_button} onClick={Back}>Back</button>
 				</section>
     </div>
   );
