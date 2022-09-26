e1c05b0b9832d2e994f8476dd2b3721cd578e56b
Daryl <ithinkmynameisdaryl@gmail.com>
2022-09-26 17:21:39 +0800

    Update API to return messages according to API doc

--- backend/urls.py
+++ backend/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('allow_session', views.allow_session, name='allowsession'),
     path('assign_doctor', views.AssignPendingExam.as_view()),
     path('submit_examination', views.AddExamination.as_view()),
-    path('view_records', views.GetExamination.as_view())
+    path('doctorviewrecords', views.DoctorGetRecords.as_view())
     #path('assign_doctor', views.assign_doctor, name='assigndoctor'),
     #path('submit_examination', views.finish_examine, name='submit_examination'),
     #path('get_sessions', views.get_sessions, name='getsessions')
