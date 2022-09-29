8ef1d4d65c29d97838b5e5a21b85ea31e0b27603
IsaacLai <e0540509@u.nus.edu>
2022-09-29 19:50:31 +0800

    Remove decorators

--- backend/views.py
+++ backend/views.py
@@ -6,7 +6,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
