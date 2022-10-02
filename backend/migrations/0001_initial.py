5e8b0af71bf9c7b023a8ec870a74634dd8ec51c1
limjierui <jieruilim@u.nus.edu>
2022-10-02 14:44:55 +0800

    Formatted code files with black(Python code formatter)

--- backend/migrations/0001_initial.py
+++ backend/migrations/0001_initial.py
@@ -10,98 +10,219 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
+        ("auth", "0012_alter_user_first_name_max_length"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('user_id', models.CharField(max_length=16, primary_key=True, serialize=False)),
-                ('username', models.CharField(max_length=50, unique=True)),
-                ('password', models.CharField(max_length=128)),
-                ('name', models.CharField(max_length=50)),
-                ('nric', models.CharField(max_length=9)),
-                ('contact', models.CharField(max_length=8)),
-                ('email', models.EmailField(max_length=50, unique=True)),
-                ('emailverified', models.BooleanField(default=False)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+                (
+                    "last_login",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
+                ),
+                (
+                    "is_superuser",
+                    models.BooleanField(
+                        default=False,
+                        help_text="Designates that this user has all permissions without explicitly assigning them.",
+                        verbose_name="superuser status",
+                    ),
+                ),
+                (
+                    "user_id",
+                    models.CharField(max_length=16, primary_key=True, serialize=False),
+                ),
+                ("username", models.CharField(max_length=50, unique=True)),
+                ("password", models.CharField(max_length=128)),
+                ("name", models.CharField(max_length=50)),
+                ("nric", models.CharField(max_length=9)),
+                ("contact", models.CharField(max_length=8)),
+                ("email", models.EmailField(max_length=50, unique=True)),
+                ("emailverified", models.BooleanField(default=False)),
+                (
+                    "groups",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.group",
+                        verbose_name="groups",
+                    ),
+                ),
+                (
+                    "user_permissions",
+                    models.ManyToManyField(
+                        blank=True,
+                        help_text="Specific permissions for this user.",
+                        related_name="user_set",
+                        related_query_name="user",
+                        to="auth.permission",
+                        verbose_name="user permissions",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='Crowd',
+            name="Crowd",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('time_recorded', models.DateTimeField(auto_now_add=True)),
-                ('count', models.IntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("time_recorded", models.DateTimeField(auto_now_add=True)),
+                ("count", models.IntegerField()),
             ],
         ),
         migrations.CreateModel(
-            name='Diagnosis',
+            name="Diagnosis",
             fields=[
-                ('code', models.CharField(max_length=10, primary_key=True, serialize=False)),
-                ('description', models.TextField()),
+                (
+                    "code",
+                    models.CharField(max_length=10, primary_key=True, serialize=False),
+                ),
+                ("description", models.TextField()),
             ],
         ),
         migrations.CreateModel(
-            name='Doctor',
+            name="Doctor",
             fields=[
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        primary_key=True,
+                        serialize=False,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='MedicalStaff',
+            name="MedicalStaff",
             fields=[
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        primary_key=True,
+                        serialize=False,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Patient',
+            name="Patient",
             fields=[
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        primary_key=True,
+                        serialize=False,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Researcher',
+            name="Researcher",
             fields=[
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        primary_key=True,
+                        serialize=False,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='HealthRecord',
+            name="HealthRecord",
             fields=[
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to='backend.patient')),
-                ('dateofbirth', models.DateField()),
-                ('height', models.IntegerField()),
-                ('weight', models.IntegerField()),
-                ('bloodtype', models.CharField(max_length=3)),
-                ('allergies', models.CharField(max_length=50)),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        primary_key=True,
+                        serialize=False,
+                        to="backend.patient",
+                    ),
+                ),
+                ("dateofbirth", models.DateField()),
+                ("height", models.IntegerField()),
+                ("weight", models.IntegerField()),
+                ("bloodtype", models.CharField(max_length=3)),
+                ("allergies", models.CharField(max_length=50)),
             ],
         ),
         migrations.CreateModel(
-            name='Examination',
+            name="Examination",
             fields=[
-                ('exam_id', models.CharField(max_length=50, primary_key=True, serialize=False)),
-                ('prescription', models.CharField(max_length=50)),
-                ('examtime', models.DateTimeField(auto_now_add=True)),
-                ('diagnosis', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='backend.diagnosis')),
-                ('doctor', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='backend.doctor')),
-                ('patient', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='backend.patient')),
+                (
+                    "exam_id",
+                    models.CharField(max_length=50, primary_key=True, serialize=False),
+                ),
+                ("prescription", models.CharField(max_length=50)),
+                ("examtime", models.DateTimeField(auto_now_add=True)),
+                (
+                    "diagnosis",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="backend.diagnosis",
+                    ),
+                ),
+                (
+                    "doctor",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="backend.doctor"
+                    ),
+                ),
+                (
+                    "patient",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="backend.patient",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='PendingExamination',
+            name="PendingExamination",
             fields=[
-                ('patient', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to='backend.patient')),
-                ('exam_id', models.CharField(max_length=10, unique=True)),
-                ('approved', models.BooleanField(default=False)),
-                ('doctor', models.OneToOneField(null=True, on_delete=django.db.models.deletion.CASCADE, to='backend.doctor')),
+                (
+                    "patient",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        primary_key=True,
+                        serialize=False,
+                        to="backend.patient",
+                    ),
+                ),
+                ("exam_id", models.CharField(max_length=10, unique=True)),
+                ("approved", models.BooleanField(default=False)),
+                (
+                    "doctor",
+                    models.OneToOneField(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="backend.doctor",
+                    ),
+                ),
             ],
         ),
     ]
