2f6ea0c5be5fb6030e8580122f4a9a5bb251b9de
Oscar Lai <77140576+oscarlai1998@users.noreply.github.com>
2022-10-11 22:43:55 +0800

    Implemented 2FA

---
+++ frontend/src/Login/Login_CreateAuth.js
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useState } from "react";
+import Login from "./Components/Login";
+import Create_Authenticator from "./Components/Create_Authenticator";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+  const [errMsg, setErrMsg] = useState("");
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: "24px", // keep right padding when drawer closed
+            }}
+          >
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              MediBook
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Grid container spacing={3}>
+            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+              <Create_Authenticator></Create_Authenticator>
+            </Paper>
+          </Grid>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default function Login_CreateAuth() {
+  return <DashboardContent />;
+}
