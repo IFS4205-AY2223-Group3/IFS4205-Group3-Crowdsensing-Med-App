e47963f58aa76cab2a393f4084a7282ab6552303
limjierui <jieruilim@u.nus.edu>
2022-10-15 22:33:19 +0800

    test pre-commit

---
+++ IOT/main2.py
@@ -0,0 +1,16 @@
+from Detector import Detector
+
+
+def main():
+    videopath = 0  # change to 0 for webcam
+
+    configpath = "model_data/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+    modelpath = "model_data/frozen_inference_graph.pb"
+    classespath = "model_data/coco.names"
+
+    detector = Detector(videopath, configpath, modelpath, classespath)
+    detector.onVideo()
+
+
+if __name__ == "__main__":
+    main()
