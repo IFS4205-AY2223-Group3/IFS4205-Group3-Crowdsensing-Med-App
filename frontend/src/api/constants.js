039df4effa2e92f80fdbda30c591eccddfa7fcb6
Alicia Tay <e0424736@u.nus.edu>
2022-10-27 12:30:41 +0800

    Fix constants.js

--- frontend/src/api/constants.js
+++ frontend/src/api/constants.js
@@ -1,28 +1,28 @@
 export const LOGIN_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/login";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/login";
 export const LOGIN_AUTH_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/createotp";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/createotp";
 export const LOGIN_OTP_URL =
-  "http://ifs4205-group3-backend-i.comp.nus.edu.sg/verifyotp";
+  "http://ifs4205-group3-backend.comp.nus.edu.sg/verifyotp";
 export const LOGOUT_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/logout";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/logout";
 export const CHANGE_OTP_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/deleteotp";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/deleteotp";
 export const GENERATE_SESSION_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/generatesession";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/generatesession";
 export const ALLOWS_SESSION_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/allowsession";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/allowsession";
 export const VIEW_RECORDS_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/patientviewrecords";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/patientviewrecords";
 export const EXAMINE_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/assigndoctor";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/assigndoctor";
 export const DOCTOR_SUBMIT_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/submitexamination";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/submitexamination";
 export const DOCTOR_VIEW_HEALTH_RECORDS_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/doctorviewrecords";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/doctorviewrecords";
 export const DOCTOR_VIEW_OLD_SESSIONS_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/doctorviewoldsessions";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/doctorviewoldsessions";
 export const VIEW_COUNT_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/iot";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/iot";
 export const VIEW_ANON_DATA_URL =
-  "https://ifs4205-group3-backend-i.comp.nus.edu.sg/researcherviewrecords";
+  "https://ifs4205-group3-backend.comp.nus.edu.sg/researcherviewrecords";
