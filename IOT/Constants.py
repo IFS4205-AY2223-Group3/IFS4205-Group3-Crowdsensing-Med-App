91b824e3a81480d2af0ce9b08e7dfb9efacb7df0
limjierui <jieruilim@u.nus.edu>
2022-10-27 20:30:54 +0800

    Edit IOT files and .gitignore

--- IOT/Constants.py
+++ IOT/Constants.py
@@ -1,6 +1,6 @@
-URL = "https://ifs4205-group3-backend-i.comp.nus.edu.sg/iot"
+URL = "https://ifs4205-group3-backend.comp.nus.edu.sg/iot"
 HEADER_ACCEPT = "application/json"
 HEADER_CONTENT_TYPE = "application/json"
-TOTAL_TIME = 30
+TOTAL_TIME = 600
 SEED_NUMBER = 20
-API_TOKEN = "44485046b0891f6ebdb81303f7bd35961cc48b6a"
+API_TOKEN = "3f458fc6e2d454a8dd09e197caab18dd8ac04bd0"
